a514a4bd4ac116f446806d15b6a469feca28b4cf
Aidana Khankel <aidanakhankeldieva91@gmail.com>
2020-09-04 21:40:52 -0500

    made some changes

--- js/app.js
+++ js/app.js
@@ -13,4 +13,42 @@
 // 4. Optional: You can show all images in small format under main image and store them inside "carousel" div.
 // This option is very broad that you can im plement so that when each small images clicked show it on main image div.
 // You can even implement border highlight(or any other effect) to specific small image when it is active on main div.
-// aidana
+
+
+
+//add array of pictures
+var container = document.querySelector('.container');
+console.log(container);
+var carName = document.querySelectorAll('.car-name');
+var imageContainer = document.querySelector('.img-container');
+var a = document.querySelectorAll('.btn');
+var iconLeft = document.querySelector('#icon-left');
+var iconRight = document.querySelector('#icon-right');
+var playWrapper = document.querySelector('.play-wrapper');
+var stopBtn = document.querySelector('.stopBtn');
+var playBtn = document.querySelector('.playBtn');
+var carousel = document.querySelector('.carousel');
+
+var images = ['./img/contBcg-0.jpeg','./img/contBcg-1.jpeg','./img/contBcg-2.jpeg','./img/contBcg-3.jpeg','./img/contBcg-4.jpeg',];
+
+
+
+
+images.forEach(img =>{
+    var imgTag = document.createElement('img');
+    carousel.appendChild(imgTag);
+    imgTag.setAttribute("src", img);
+
+});
+
+images.forEach(img => {
+    var imgTag = document.createElement('img');
+    imageContainer.appendChild(imgTag);
+    imgTag.setAttribute("src", img);
+
+});
+
+
+
+
+

9c27e188f83338bca5775ff41e2414cc73cb5cf0
tal-dev <talgat.abykeev@gmail.com>
2020-09-04 21:04:54 -0500

    slider-cars

--- js/app.js
+++ js/app.js
@@ -13,3 +13,81 @@
 // 4. Optional: You can show all images in small format under main image and store them inside "carousel" div.
 // This option is very broad that you can im plement so that when each small images clicked show it on main image div.
 // You can even implement border highlight(or any other effect) to specific small image when it is active on main div.
+
+let cars = [
+    "./img/contBcg-0.jpeg",
+    "./img/contBcg-1.jpeg",
+    "./img/contBcg-2.jpeg",
+    "./img/contBcg-3.jpeg",
+    "./img/contBcg-4.jpeg",
+];
+
+let iconRight = document.querySelector('#icon-right');
+let iconLeft = document.querySelector('#icon-left');
+let imageContainer = document.querySelector('.img-container');
+let sliderCountStart = 1;
+let sliderCountEnd = 4;
+let playBtn = document.querySelector('#playBtn');
+let stopBtn = document.querySelector('#stopBtn');
+let carousel = document.querySelector('.carousel');
+
+imageContainer.addEventListener('click', (e) => {
+
+    if (e.target.id == 'icon-right') {
+        imageContainer.style.background = "url('./img/contBcg-" + sliderCountStart + ".jpeg') center/cover no-repeat";
+        sliderCountStart++;
+
+        if (sliderCountStart == 5) {
+            sliderCountStart = 0;
+        }
+    } else if (e.target.id == 'icon-left') {
+        imageContainer.style.background = "url('./img/contBcg-" + sliderCountEnd + ".jpeg') center/cover no-repeat";
+        sliderCountEnd--;
+
+        if (sliderCountEnd == -1) {
+            imageContainer.style.background = "url('./img/contBcg-" + 0 + ".jpeg') center/cover no-repeat";
+            sliderCountEnd = 4;
+        }
+    }
+})
+
+function slideRight(index) {
+    imageContainer.style.background = "url('./img/contBcg-" + index + ".jpeg') center/cover no-repeat";
+    index++;
+}
+
+playBtn.addEventListener('click', (e) => {
+    
+    let count = 0;
+    let right = setInterval((slideRight) => {     
+        imageContainer.style.background = "url('./img/contBcg-" + count + ".jpeg') center/cover no-repeat"; 
+        count++;
+        if(count == 5) {
+            count = 0;
+        }
+    }, 1000);
+
+    stopBtn.addEventListener('click', (e) => {
+        clearInterval(right)
+    })
+})
+
+function createCarouselBox(index) {
+    let div = document.createElement('div');
+    div.style.width = '250px';
+    div.style.height = '200px';
+    div.style.background = "url('./img/contBcg-" + index + ".jpeg') center/cover no-repeat";
+    div.style.display = 'inline-block'
+    div.style.margin = '0 10px'
+    div.id = index;
+    carousel.appendChild(div);
+}
+
+for(let i = 0; i < cars.length; i++) {
+    createCarouselBox(i);
+}
+
+carousel.addEventListener('click', (e) => {
+    console.log(e.target.id)
+    imageContainer.style.background = "url('./img/contBcg-" + e.target.id + ".jpeg') center/cover no-repeat";
+})

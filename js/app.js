e4ce2aca4a2b5e6123d7b17af77fa29b65866714
Askat B <Askat@MacBook-Pro-2.home>
2020-09-05 00:33:05 -0400

    carousel images and animation

--- js/app.js
+++ js/app.js
@@ -5,41 +5,44 @@
 // STEPS TO TAKE:
 
 // 1. Make sure manual slider works when we press left or right buttons.
+// 2. Make sure you see each car's name on top of the each image. The names should change when you click left or right buttons accordingly.
+
+// 3. Then try to implement automatic slider using start/stop buttons. hint: setInterval, clearInterval
 var cars = [
   {
     id: 0,
     title: "Mercedes BENZ AMG",
     model: "C 43",
     info: "AMG-enhanced 3.0L V6 biturbo",
-    imgUrl: "url(./img/contBcg-0.jpeg)",
+    imgUrl: "./img/contBcg-0.jpeg",
   },
   {
     id: 1,
     title: "Mercedes BENZ AMG",
     model: "E 63 S",
     info: "Handcrafted AMG 4.0L V8 biturbo",
-    imgUrl: "url(./img/contBcg-1.jpeg)",
+    imgUrl: "./img/contBcg-1.jpeg",
   },
   {
     id: 2,
     title: "BMW",
     model: "M550i",
     info: "4.4-liter BMW M TwinPower Turbo V-8 engine",
-    imgUrl: "url(./img/contBcg-2.jpeg)",
+    imgUrl: "./img/contBcg-2.jpeg",
   },
   {
     id: 3,
     title: "AUDI",
     model: "TT RS Coupe Trims",
     info: "2.5-liter five-cylinder",
-    imgUrl: "url(./img/contBcg-3.jpeg)",
+    imgUrl: "./img/contBcg-3.jpeg",
   },
   {
     id: 4,
     title: "Dodge Challenger",
     model: "SRT Hellcat",
     info: "6.2-liter v8 OHV",
-    imgUrl: "url(./img/contBcg-4.jpeg)",
+    imgUrl: "./img/contBcg-4.jpeg",
   },
 ];
 
@@ -50,15 +53,27 @@ var img = document.querySelector(".img-container");
 var title = document.querySelector(".car-name");
 var playBtn = document.querySelector("#playBtn");
 var stopBtn = document.querySelector("#stopBtn");
+var carousel = document.querySelector(".carousel");
+
+// SHOW CAROUSEL IMAGES
+cars.forEach(function (car) {
+  var img = document.createElement("img");
+  img.setAttribute("data-id", car.id);
+  img.classList.add("carousel-img");
+  img.src = car.imgUrl;
+  carousel.appendChild(img);
+});
+
+var carouselImg = document.querySelectorAll(".carousel-img");
 
 // HELPER FUNCTIONS
 function slideShow(data, index) {
   data.forEach(function (car, i) {
     if (index === i) {
-      img.style.backgroundImage = car.imgUrl;
+      img.style.backgroundImage = `url(${car.imgUrl})`; //"url(./img/contBcg-4.jpeg)"
       title.textContent = car.title;
+      carouselActiveImg(index);
     }
-    // console.log(car);
   });
 }
 
@@ -86,11 +101,28 @@ function nextSlide() {
 
 var playSlide = null;
 function playSlideShow() {
-  playSlide = setInterval(nextSlide, 1000);
+  playSlide = setInterval(nextSlide, 2000);
 }
 
 function stopSlideShow() {
   clearInterval(playSlide);
+}
+
+function changeImg(e) {
+  cars.forEach(function (car) {
+    if (e.target.getAttribute("data-id") == car.id) {
+      slideShow(cars, car.id);
+    }
+  });
+}
+
+function carouselActiveImg(index) {
+  carouselImg.forEach(function (img) {
+    img.classList.remove("active");
+    if (index == img.getAttribute("data-id")) {
+      img.classList.add("active");
+    }
+  });
 }
 
 // FIRST SLIDE BY DEFAULT
@@ -106,9 +138,7 @@ playBtn.addEventListener("click", playSlideShow);
 
 stopBtn.addEventListener("click", stopSlideShow);
 
-// 2. Make sure you see each car's name on top of the each image. The names should change when you click left or right buttons accordingly.
-
-// 3. Then try to implement automatic slider using start/stop buttons. hint: setInterval, clearInterval
+carousel.addEventListener("click", changeImg);
 
 // 4. Optional: You can show all images in small format under main image and store them inside "carousel" div.
 // This option is very broad that you can im plement so that when each small images clicked show it on main image div.

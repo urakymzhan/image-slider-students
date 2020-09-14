458da6b1b5710e880c70072cdef9dd9c4bc56be6
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-09-14 15:20:13 -0500

    feedback

--- js/app.js
+++ js/app.js
@@ -14,8 +14,6 @@
 // This option is very broad that you can im plement so that when each small images clicked show it on main image div.
 // You can even implement border highlight(or any other effect) to specific small image when it is active on main div.
 
-
-
 //add array of pictures
 var container = document.querySelector('.container');
 console.log(container);
@@ -25,21 +23,23 @@ var a = document.querySelectorAll('.btn');
 var iconLeft = document.querySelector('#icon-left');
 var iconRight = document.querySelector('#icon-right');
 var playWrapper = document.querySelector('.play-wrapper');
-var stopBtn = document.querySelector('.stopBtn');
+var stopBtn = document.querySelector('#stopBtn');
 var playBtn = document.querySelector('#playBtn');
 var carousel = document.querySelector('.carousel');
 
-var images = ['./img/contBcg-0.jpeg','./img/contBcg-1.jpeg','./img/contBcg-2.jpeg','./img/contBcg-3.jpeg','./img/contBcg-4.jpeg',];
+var images = [
+  './img/contBcg-0.jpeg',
+  './img/contBcg-1.jpeg',
+  './img/contBcg-2.jpeg',
+  './img/contBcg-3.jpeg',
+  './img/contBcg-4.jpeg',
+];
 
-
-
-
-images.forEach(img =>{
-    var imgTag = document.createElement('img');
-    carousel.appendChild(imgTag);
-    imgTag.setAttribute("src", img);
-
-});
+// images.forEach((img) => {
+//   var imgTag = document.createElement('img');
+//   carousel.appendChild(imgTag);
+//   imgTag.setAttribute('src', img);
+// });
 
 // images.forEach(img => {
 //     var imgTag = document.createElement('img');
@@ -49,49 +49,89 @@ images.forEach(img =>{
 // });
 
 //function iconRightFunc(){
-    //debugger
-    function rightArrow(){
-var counter1 = 1;
-iconRight.addEventListener("click", function(){
-    if(counter1 === 5){
-        counter1=0;
-    }
-    imageContainer.style.backgroundImage = `url(${images[counter1]})`;
-    counter1++;
-});
-console.log(images[counter1]);
-return counter1;
-
-    }
-
+//debugger
+// function rightArrow() {
+//   var counter1 = 1;
+//   iconRight.addEventListener('click', function () {
+//     if (counter1 === 5) {
+//       counter1 = 0;
+//     }
+//     imageContainer.style.backgroundImage = `url(${images[counter1]})`;
+//     counter1++;
+//   });
+//   console.log(images[counter1]);
+//   return counter1;
+// }
 
 //left arrow function
 
-let counter2 = 1;
-iconLeft.addEventListener("click", function(){
-    if(counter2 < 0){
-        counter2 = images.length-1;
-    }
-    imageContainer.style.backgroundImage = `url(${images[counter2]})`;
-    counter2--;
+// let counter2 = 1;
+// iconLeft.addEventListener('click', function () {
+//   if (counter2 < 0) {
+//     counter2 = images.length - 1;
+//   }
+//   imageContainer.style.backgroundImage = `url(${images[counter2]})`;
+//   counter2--;
+// });
+
+// function imgSlide() {
+//   playBtn.addEventListener('click', function () {
+//     let counter = 0;
+
+//     imageContainer.style.backgroundImage = `url(${images[counter]})`;
+//     if (counter < images.length - 1) {
+//       counter++;
+//     } else {
+//       counter = 0;
+//     }
+
+//     setTimeout('imgSlide()', 2000);
+//   });
+// }
+
+// **ULAN NOTE** Your code is up^
+// BELOW IS MY MODIFICATIONS TO YOUR CODE
+var counter = 0;
+
+images.forEach((img, index) => {
+  var imgTag = document.createElement('img');
+  carousel.appendChild(imgTag);
+  imgTag.setAttribute('src', img);
+  imgTag.addEventListener('click', function () {
+    counter = index;
+    imageContainer.style.backgroundImage = `url(${images[index]})`;
+  });
 });
 
-function imgSlide(){
- playBtn.addEventListener("click", function(){
-  
-    let counter = 0;
-        
+iconRight.addEventListener('click', function () {
+  counter++;
+  if (counter === images.length) {
+    counter = 0;
+  }
+  imageContainer.style.backgroundImage = `url(${images[counter]})`;
+});
+
+iconLeft.addEventListener('click', function () {
+  if (counter === 0) {
+    counter = images.length;
+  }
+  counter--;
+  imageContainer.style.backgroundImage = `url(${images[counter]})`;
+});
+
+var timer;
+playBtn.addEventListener('click', function () {
+  timer = setInterval(function () {
+    counter++;
+    if (counter === images.length) {
+      counter = 0;
+    }
     imageContainer.style.backgroundImage = `url(${images[counter]})`;
-    if(counter < images.length-1){
-        counter++;
-    }else{
-        counter = 0;
-    }
-    
-    
-    setTimeout("imgSlide()", 2000);
+  }, 1000);
 });
-}
-    
 
+stopBtn.addEventListener('click', function () {
+  clearInterval(timer);
+});
 
+// Here you can add more functionalities...

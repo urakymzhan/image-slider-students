c6d3b7a92373c451b7013be17898a1ce90d7aa9d
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-09-16 11:21:13 -0500

    feedback

--- js/app.js
+++ js/app.js
@@ -16,159 +16,249 @@
 
 // CREATE NEW ARRAY OF CARS
 var bgImgData = [
-    {
-        id: 1,
-        name: 'Bugatti',
-        imageUrl: './img/bugatti.png'
-    },
-    {
-        id: 2,
-        name: 'Mercedes Benz',
-        imageUrl: './img/mercedes.png'
-    },
-    {
-        id: 3,
-        name: 'Ferrari',
-        imageUrl: './img/ferrari.png'
-    },
-    {
-        id: 4,
-        name: 'Audi R8 V10',
-        imageUrl: './img/audi.png'
-    },
-    {
-        id: 5,
-        name: 'BMW',
-        imageUrl: './img/bmw.png'
-    },
-    {
-        id: 6,
-        name: 'Lamborghini Aventador',
-        imageUrl: './img/lamborghini.png'
-    },
-    {
-        id: 7,
-        name: 'Porsche Gt3',
-        imageUrl: './img/porsche.png'
-    },
-    {
-        id: 8,
-        name: 'Nissan GT-R',
-        imageUrl: './img/nissan.png'
-    }
-]
+  {
+    id: 1,
+    name: 'Bugatti',
+    imageUrl: './img/bugatti.png',
+  },
+  {
+    id: 2,
+    name: 'Mercedes Benz',
+    imageUrl: './img/mercedes.png',
+  },
+  {
+    id: 3,
+    name: 'Ferrari',
+    imageUrl: './img/ferrari.png',
+  },
+  {
+    id: 4,
+    name: 'Audi R8 V10',
+    imageUrl: './img/audi.png',
+  },
+  {
+    id: 5,
+    name: 'BMW',
+    imageUrl: './img/bmw.png',
+  },
+  {
+    id: 6,
+    name: 'Lamborghini Aventador',
+    imageUrl: './img/lamborghini.png',
+  },
+  {
+    id: 7,
+    name: 'Porsche Gt3',
+    imageUrl: './img/porsche.png',
+  },
+  {
+    id: 8,
+    name: 'Nissan GT-R',
+    imageUrl: './img/nissan.png',
+  },
+];
 
 // IMPORT HTML ELEMENTS TO JS
 var imgContainer = document.querySelector('.img-container');
 var carouselContent = document.querySelector('.carousel');
 var carName = document.querySelector('.car-name');
-var btnLeft = document.querySelector('.btn-left')
-var btnRight = document.querySelector('.btn-right')
-var playButton = document.getElementById('playBtn')
-var stopButton = document.getElementById('stopBtn')
-var containerDiv = document.querySelector('.container')
-var carOverlapName = document.querySelector('.text')
+var btnLeft = document.querySelector('.btn-left');
+var btnRight = document.querySelector('.btn-right');
+var playButton = document.getElementById('playBtn');
+var stopButton = document.getElementById('stopBtn');
+var containerDiv = document.querySelector('.container');
+var carOverlapName = document.querySelector('.text');
 
-// CREATE A NEW DATA OBJECT OUT OF ARRAY (bgImgData) OF CARS
+// // CREATE A NEW DATA OBJECT OUT OF ARRAY (bgImgData) OF CARS
+// var keysBgImgData = {};
+
+// // MAKE SURE THAT ARRAY (bgImgData) OF CARS IS NOT EMPTY
+// if(bgImgData.length > 0){
+//     bgImgData.forEach(function(item){
+//         keysBgImgData[item.id] = item;
+//         var imgTab = document.createElement('img');
+//         imgTab.src = item.imageUrl;
+//         imgTab.setAttribute('data-id', item.id);
+//         carouselContent.appendChild(imgTab)
+//     })
+// } else {
+//     carName.textContent = 'There is no car images'
+// }
+
+// // OUTPUT OF keysBgImgData
+// /*
+// keysBgImgData = {1: {…}, 2: {…}, 3: {…}, 4: {…}, 5: {…}, 6: {…}, 7: {…}, 8: {…}};
+// keysBgImgData = {
+//         1: {id: 1, name: "Bugatti", imageUrl: "./img/bugatti.png"},
+//         2: {id: 2, name: "Mercedes Benz", imageUrl: "./img/mercedes.png"},
+//         3: {id: 3, name: "Ferrari", imageUrl: "./img/ferrari.png"},
+//         4: {id: 4, name: "Audi R8 V10", imageUrl: "./img/audi.png"},
+//         5: {id: 5, name: "BMW", imageUrl: "./img/bmw.png"},
+//         6: {id: 6, name: "Lamborghini Aventador", imageUrl: "./img/lamborghini.png"},
+//         7: {id: 7, name: "Porsche Gt3", imageUrl: "./img/porsche.png"},
+//         8: {id: 8, name: "Nissan GT-R", imageUrl: "./img/nissan.png"}
+// }
+// */
+
+// var count = 1;
+// var timerCount = 0, timerClear;
+
+// // ADD EVENT LISTENER TO THE WHOLE CONTAINER (event.currentTarget - WHERE THE EVENT IS LISTENED), (event.target - WHERE THE EVENT IS HAPPENED)
+// containerDiv.addEventListener('click', function(event){
+
+//     // GET data-id ATTRIBUTE FROM ANY CLICKED ELEMENT INSIDE THE ELEMENT WITH .container CLASS
+//     var imgDataId = event.target.getAttribute('data-id');
+
+//     // MAKE SURE THAT CLICKED ELEMENT'S data-id IS NOT EQUAL TO null
+//     if(imgDataId !== null){
+//         carName.textContent = keysBgImgData[imgDataId].name
+//         imgContainer.style.background = "url('"+keysBgImgData[imgDataId].imageUrl+"') center/cover no-repeat"
+//         carOverlapName.textContent = keysBgImgData[imgDataId].name
+//     }
+
+//     // THIS SECTION WILL KICK IN WHEN YOU PRESS "icon_right" BUTTON/ICON
+//     if(event.target.getAttribute('id') === 'icon-right'){
+//         carName.textContent = keysBgImgData[count].name
+//         imgContainer.style.background = "url('"+keysBgImgData[count].imageUrl+"') center/cover no-repeat"
+//         carOverlapName.textContent = keysBgImgData[count].name
+//         for(var item of carouselContent.children){
+//             item.classList.remove('active')
+//         }
+//         carouselContent.children[count-1].setAttribute('class', 'active')
+//         count++
+//         if(count > bgImgData.length){
+//             count = 1
+//         }
+//     }
+
+//     // THIS SECTION WILL KICK IN WHEN YOU PRESS "icon_left" BUTTON/ICON
+//     if(event.target.getAttribute('id') === 'icon-left'){
+//         if(count <= 0){
+//             count = bgImgData.length
+//         }
+//         carName.textContent = keysBgImgData[count].name
+//         imgContainer.style.background = "url('"+keysBgImgData[count].imageUrl+"') center/cover no-repeat"
+//         carOverlapName.textContent = keysBgImgData[count].name
+//         for(var item of carouselContent.children){
+//             item.classList.remove('active')
+//         }
+//         carouselContent.children[count-1].setAttribute('class', 'active')
+//         count--
+//     }
+
+//     // THIS SECTION WILL KICK IN WHEN YOU PRESS "playBtn" BUTTON/ICON
+//     if(event.target.getAttribute('id') === 'playBtn'){
+//         timerClear = setInterval(setImgInterval, 2000);
+//     }
+
+//     // THIS SECTION WILL KICK IN WHEN YOU PRESS "stopBtn" BUTTON/ICON
+//     if(event.target.getAttribute('id') === 'stopBtn'){
+//         clearInterval(timerClear)
+//     }
+
+// })
+
+// // THIS IS THE FUNCTION WHICH EXECUTES WHEN YOU PRESS "playBtn"
+// function setImgInterval(){
+//     timerCount++;
+//     carName.textContent = keysBgImgData[timerCount].name
+//     imgContainer.style.background = "url('"+keysBgImgData[timerCount].imageUrl+"') center/cover no-repeat"
+//     carOverlapName.textContent = keysBgImgData[timerCount].name
+//     for(var item of carouselContent.children){
+//         item.classList.remove('active')
+//     }
+//     carouselContent.children[timerCount-1].setAttribute('class', 'active')
+//     if(timerCount > bgImgData.length-1){
+//         timerCount = 0
+//     }
+// }
+
+// ** ULANS NOTE BELOW **
+// deleted carNames. No need to output both: carOverlapName and carNames.
+// ideally better use only one counter: either count or timerCount
+
 var keysBgImgData = {};
 
-// MAKE SURE THAT ARRAY (bgImgData) OF CARS IS NOT EMPTY
-if(bgImgData.length > 0){
-    bgImgData.forEach(function(item){
-        keysBgImgData[item.id] = item;
-        var imgTab = document.createElement('img');
-        imgTab.src = item.imageUrl;
-        imgTab.setAttribute('data-id', item.id);
-        carouselContent.appendChild(imgTab)
-    })
+// make sure bgImgData is not empty
+if (bgImgData.length > 0) {
+  bgImgData.forEach(function (item) {
+    keysBgImgData[item.id] = item;
+    var imgTab = document.createElement('img');
+    imgTab.src = item.imageUrl;
+    imgTab.setAttribute('data-id', item.id);
+    carouselContent.appendChild(imgTab);
+  });
 } else {
-    carName.textContent = 'There is no car images'
+  carName.textContent = 'There is no car images';
 }
-
-// OUTPUT OF keysBgImgData
-/* 
-keysBgImgData = {1: {…}, 2: {…}, 3: {…}, 4: {…}, 5: {…}, 6: {…}, 7: {…}, 8: {…}};
-keysBgImgData = {
-        1: {id: 1, name: "Bugatti", imageUrl: "./img/bugatti.png"},
-        2: {id: 2, name: "Mercedes Benz", imageUrl: "./img/mercedes.png"},
-        3: {id: 3, name: "Ferrari", imageUrl: "./img/ferrari.png"},
-        4: {id: 4, name: "Audi R8 V10", imageUrl: "./img/audi.png"},
-        5: {id: 5, name: "BMW", imageUrl: "./img/bmw.png"},
-        6: {id: 6, name: "Lamborghini Aventador", imageUrl: "./img/lamborghini.png"},
-        7: {id: 7, name: "Porsche Gt3", imageUrl: "./img/porsche.png"},
-        8: {id: 8, name: "Nissan GT-R", imageUrl: "./img/nissan.png"}
-}
-*/
+// inital data
+imgContainer.style.background = `url(${bgImgData[0].imageUrl}) center/cover no-repeat`;
+carOverlapName.textContent = bgImgData[0].name;
+carouselContent.children[0].setAttribute('class', 'active');
 
 var count = 1;
-var timerCount = 0, timerClear;
+var timerCount = 0,
+  timerClear;
 
-// ADD EVENT LISTENER TO THE WHOLE CONTAINER (event.currentTarget - WHERE THE EVENT IS LISTENED), (event.target - WHERE THE EVENT IS HAPPENED)
-containerDiv.addEventListener('click', function(event){
-    
-    // GET data-id ATTRIBUTE FROM ANY CLICKED ELEMENT INSIDE THE ELEMENT WITH .container CLASS
-    var imgDataId = event.target.getAttribute('data-id');
+containerDiv.addEventListener('click', function (event) {
+  var imgId = event.target.getAttribute('data-id');
 
-    // MAKE SURE THAT CLICKED ELEMENT'S data-id IS NOT EQUAL TO null
-    if(imgDataId !== null){
-        carName.textContent = keysBgImgData[imgDataId].name
-        imgContainer.style.background = "url('"+keysBgImgData[imgDataId].imageUrl+"') center/cover no-repeat"
-        carOverlapName.textContent = keysBgImgData[imgDataId].name
-    }
+  // make sure data-id is not null
+  if (imgId !== null) {
+    setBgImageAndName(imgId);
+  }
 
-    // THIS SECTION WILL KICK IN WHEN YOU PRESS "icon_right" BUTTON/ICON
-    if(event.target.getAttribute('id') === 'icon-right'){
-        carName.textContent = keysBgImgData[count].name
-        imgContainer.style.background = "url('"+keysBgImgData[count].imageUrl+"') center/cover no-repeat"
-        carOverlapName.textContent = keysBgImgData[count].name
-        for(var item of carouselContent.children){
-            item.classList.remove('active')
-        }
-        carouselContent.children[count-1].setAttribute('class', 'active')
-        count++
-        if(count > bgImgData.length){
-            count = 1
-        }
-    }
+  // icon_right button
+  if (event.target.getAttribute('id') === 'icon-right') {
+    count++;
+    setBgImageAndName(count);
+    setActive(count);
+  }
+  if (count === bgImgData.length) {
+    count = 0;
+  }
 
-    // THIS SECTION WILL KICK IN WHEN YOU PRESS "icon_left" BUTTON/ICON
-    if(event.target.getAttribute('id') === 'icon-left'){
-        if(count <= 0){
-            count = bgImgData.length
-        }
-        carName.textContent = keysBgImgData[count].name
-        imgContainer.style.background = "url('"+keysBgImgData[count].imageUrl+"') center/cover no-repeat"
-        carOverlapName.textContent = keysBgImgData[count].name
-        for(var item of carouselContent.children){
-            item.classList.remove('active')
-        }
-        carouselContent.children[count-1].setAttribute('class', 'active')
-        count--
+  // icon_left button
+  if (event.target.getAttribute('id') === 'icon-left') {
+    if (count <= 0) {
+      count = bgImgData.length;
     }
-    
-    // THIS SECTION WILL KICK IN WHEN YOU PRESS "playBtn" BUTTON/ICON
-    if(event.target.getAttribute('id') === 'playBtn'){
-        timerClear = setInterval(setImgInterval, 2000);
-    }
+    setBgImageAndName(count);
+    setActive(count);
+    count--;
+  }
 
-    // THIS SECTION WILL KICK IN WHEN YOU PRESS "stopBtn" BUTTON/ICON
-    if(event.target.getAttribute('id') === 'stopBtn'){
-        clearInterval(timerClear)
-    }
+  // playBtn button
+  if (event.target.getAttribute('id') === 'playBtn') {
+    timerClear = setInterval(setImgInterval, 2000);
+  }
 
-})
+  // stopBtn button
+  if (event.target.getAttribute('id') === 'stopBtn') {
+    clearInterval(timerClear);
+  }
+});
 
-// THIS IS THE FUNCTION WHICH EXECUTES WHEN YOU PRESS "playBtn"
-function setImgInterval(){
-    timerCount++;
-    carName.textContent = keysBgImgData[timerCount].name
-    imgContainer.style.background = "url('"+keysBgImgData[timerCount].imageUrl+"') center/cover no-repeat"
-    carOverlapName.textContent = keysBgImgData[timerCount].name
-    for(var item of carouselContent.children){
-        item.classList.remove('active')
-    }
-    carouselContent.children[timerCount-1].setAttribute('class', 'active')
-    if(timerCount > bgImgData.length-1){
-        timerCount = 0
-    }
+// helper function for playBtn
+function setImgInterval() {
+  timerCount++;
+  setBgImageAndName(timerCount);
+  setActive(timerCount);
+
+  if (timerCount > bgImgData.length - 1) {
+    timerCount = 0;
+  }
+}
+// helper function to set background image and names
+function setBgImageAndName(ind) {
+  imgContainer.style.background =
+    "url('" + keysBgImgData[ind].imageUrl + "') center/cover no-repeat";
+  carOverlapName.textContent = keysBgImgData[ind].name;
+}
+// helper function to set css active class
+function setActive(ind) {
+  for (var item of carouselContent.children) {
+    item.classList.remove('active');
+  }
+  carouselContent.children[ind - 1].setAttribute('class', 'active');
 }
